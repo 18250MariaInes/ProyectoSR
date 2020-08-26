d90838da3b317c5e5567e32396ecaa4318276f52
Maria Vasquez <maria.ines.vf@gmail.com>
2020-08-25 21:10:42 -0600

    avances en posicionar escena

--- Engine3D.py
+++ Engine3D.py
@@ -20,20 +20,32 @@ height=500
 r = Render(width,height)
 
 #rocketModel Texture
-"""r.active_texture = Texture('./models/planespace.bmp')
-r.active_shader = unlit"""
+r.active_texture = Texture('./models/planespace.bmp')
+r.active_shader = unlit
+
+posModel = ( 0, 0, -5)
+r.lookAt(posModel, (0,0,0))
+
+#rocketModel
+r.loadModel('./models/space-shuttle-orbiter.obj', (-3,0,-5), (0.002,0.002,0.002),(0,130,0))
 
 #astro texture
 r.active_texture = Texture('./models/suit.bmp')
 r.active_shader = gouraud #cambiar a gouraud
 
+#Astro
+r.loadModel('./models/astronaute.obj', (0,0,-5), (0.5,0.5,0.5),(0,0,0))
+
 #mars
 r.active_texture = Texture('./models/metal.bmp')
 r.active_shader = unlit #cambiar a gouraud
 
+#Mars robot
+r.loadModel('./models/marsRobot.obj',  (2,0,-5), (0.005,0.005,0.005),(0,90,0))
+
 #r.lightx, r.lighty, r.lightz=1,0,0
 #( 3, 0, *profundidad y direccion con -*5)
-posModel = ( 0, 0, -3)
+
 
 #high angle
 #r.lookAt(posModel, (0,2,0))
@@ -48,14 +60,11 @@ r.lookAt(posModel, (0,0,0))
 #r.lookAt(posModel, (-2,-2,-0.25))
 
 
-#rocketModel
-#r.loadModel('./models/space-shuttle-orbiter.obj', posModel, (0.002,0.002,0.002),(0,130,0))
 
-#Astro
-#r.loadModel('./models/astronaute.obj', posModel, (0.5,0.5,0.5),(0,0,0))
 
-#Mars robot
-r.loadModel('./models/marsRobot.obj', posModel, (0.010,0.010,0.010),(0,90,0))
+
+
+
 
 r.glFinish('output.bmp')
 #r.glZBuffer('zbuffer.bmp')
